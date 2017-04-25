5c15238c50138484d5ccd58bc59bbef3627af741
manoj <manoj.g@reactore.com>
2017-04-25 09:41:53 +0530

    feat:added sample angular app

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    styles: [
+
+    ],
+    template: `
+    <div class="jumbotron text-center">
+      <h1>The App Lives!</h1>
+      <p>{{ message }}</p>
+    </div>
+     `
+})
+export class AppComponent {
+    message = "Hello World";
+}
